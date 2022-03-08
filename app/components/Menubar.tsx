937ece6b37ca611ca9f9b1922b285af0409daae4
Skn1999 <sumit@optmyzr.com>
2022-03-08 20:44:44 +0530

    Menubar updated to accept user as prop

--- app/components/Menubar.tsx
+++ app/components/Menubar.tsx
@@ -1,7 +1,14 @@
-import { ReactElement } from "react";
+import { User } from "@prisma/client";
 import type { LinkProps } from "react-router-dom";
-import { Link } from "remix";
+import { Link, useLoaderData } from "remix";
 import Logo from "./Logo";
+import { ChevronDownIcon } from "@heroicons/react/solid";
+import { useCallback, useRef, useState } from "react";
+import Modal from "./Modal";
+
+type Props = {
+  user?: User | null;
+};
 
 const NavItem = ({ children, className, ...props }: LinkProps) => {
   return (
@@ -11,9 +18,23 @@ const NavItem = ({ children, className, ...props }: LinkProps) => {
   );
 };
 
-export default function Menubar() {
+export default function Menubar({ user }: Props = { user: null }) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  // const ref = useRef<HTMLDivElement | null>(null);
+  const [refState, setRefState] = useState<HTMLElement | null>(null);
+  const ref = useCallback((node) => {
+    setRefState(node);
+  }, []);
+  const [isMouseOver, setIsMouseOver] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsMouseOver(true);
+  };
+  const handleMouseExit = () => {
+    setIsMouseOver(false);
+  };
   return (
-    <nav className="relative top-5 left-auto right-auto flex items-center justify-start p-4 mx-auto rounded-md overflow-hidden shadow-2xl border-b border-indigo-500 bg-transparent">
+    <nav className="relative top-5 left-auto right-auto flex items-center justify-start p-6 mx-auto rounded-md overflow-hidden shadow-2xl border-b border-indigo-500 bg-transparent">
       <div className="absolute backdrop-blur-2xl bg-white bg-opacity-5 inset-0 w-full h-full"></div>
       <Logo />
 
@@ -26,12 +47,41 @@ export default function Menubar() {
         >
           Github
         </NavItem>
-        <NavItem
-          to="/login"
-          className="bg-white rounded-md p-2 px-3 font-medium text-indigo-900 hover:shadow-lg hover:text-indigo-900"
-        >
-          Login
-        </NavItem>
+        {user && user.id ? (
+          <form method="post" action="/logout">
+            <button
+              type="submit"
+              ref={ref}
+              className="font-sm text-white font-medium flex items-center justify-center p-4 -my-4 -mr-4 rounded hover:bg-indigo-100 hover:bg-opacity-20 transition-all duration-200 cursor-pointer group"
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseExit}
+              style={{
+                minWidth: refState?.getBoundingClientRect().width ?? 0,
+                textAlign: "center",
+              }}
+            >
+              <p>{isMouseOver ? "Logout" : "🧑‍🦱 " + user.fullName}</p>
+              {/* <span className="w-4 h-4 ml-2">
+              <ChevronDownIcon className="w-full h-full text-white" />
+            </span> */}
+              {/* <Modal
+              isOpen={isModalOpen}
+              horizontalAlign="right"
+              verticalAlign="bottom"
+              anchorRef={ref}
+            >
+              <li>Some thing</li>
+            </Modal> */}
+            </button>
+          </form>
+        ) : (
+          <NavItem
+            to="/login"
+            className="bg-white rounded-md p-2 px-3 font-medium text-indigo-900 hover:shadow-lg hover:text-indigo-900"
+          >
+            Login
+          </NavItem>
+        )}
       </ul>
 
       {/* <div className="flex items-center justify-center rounded-full border ml-8 cursor-pointer hover:bg-indigo-700 w-12 h-12 text-lg transition duration-150 border-indigo-200 relative">
